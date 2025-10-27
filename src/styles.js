0e9fc1668ab71517fa7a3e2918ef28089cebaee9
webiwo <webiwo360@gmail.com>
2025-10-27 17:55:12 +0100

    leading changed

--- src/styles.js
+++ src/styles.js
@@ -1,8 +1,8 @@
 const styles = {
   heroHeadText:
-    "font-black text-white lg:text-[50px] sm:text-[36px] xs:text-[24px] text-[22px] leading-[48px] lg:leading-[98px]",
+    "font-black text-white lg:text-[50px] sm:text-[36px] xs:text-[24px] text-[22px] leading-[32px] lg:leading-[98px]",
   heroSubText:
-    "text-[#dfd9ff] font-medium lg:text-[26px] sm:text-[22px] xs:text-[16px] text-[16px] lg:leading-[30px] mt-4",
+    "text-[#dfd9ff] font-medium lg:text-[26px] sm:text-[22px] xs:text-[16px] text-[16px] lg:leading-[30px] mt-3",
   sectionHeadText:
     "text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
   sectionSubText:
